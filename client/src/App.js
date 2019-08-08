bade83bb3f4bebb95dba806967bc6f716f0785e4
k-lia <k.liapoulos@gmail.com>
2019-08-08 16:34:03 -0500

    fixing routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,29 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Pricing from "./pages/Pricing";
+import Navigation from './components/Navigation';
 
+
+/************** PASSPORTS ************/
+// import Login from './components/Login';
+// import Logout from './components/Logout';
+// import Signup from './pages/Signup';
 
 function App() {
   return (
     <Router>
       <div>
+      <Navigation/>
         <Switch>
           {/* we can have this as the catch-all 404 page */}
           <Route exact path="/" component={Pricing} />
-          {/* <Route exact path="/books" component={Books} /> */}
-          {/* <Route exact path="/books/:id" component={Detail} /> */}
-          {/* <Route component={NoMatch} /> */}
+
+          {/* <Route path="/signup" component={Signup} /> */}
+          {/* <Route path="/login" component={Login} /> */}
+          {/* <Route path="/logout" component={Logout} /> */}
+
         </Switch>
+        
       </div>
     </Router>
   );
