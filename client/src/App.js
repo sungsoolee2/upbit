ca26d7b6ae38bec2db116c8faa19871a5d3f1995
Joey Lansangan <joeylansangan@gmail.com>
2019-08-10 15:13:13 -0500

    styled navbar and pricing page

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,12 @@ import Pricing from "./pages/Pricing";
 import Navigation from './components/Navigation/index';
 import Container from './styles/container.css'
 import Home from "./pages/Home";
-// import PrivateRoute from './PrivateRoute'
+import PrivateRoute from './PrivateRoute'
 
 /************** PASSPORTS ************/
-// import Login from './components/Login';
-// import Logout from './components/Logout';
-// import Signup from './pages/Signup';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import Signup from './pages/Signup';
 
 function App() {
   return (
@@ -21,9 +21,9 @@ function App() {
           {/* we can have this as the catch-all 404 page */}
           <Route exact path="/" component={Home} />
           <Route exact path="/pricing" component={Pricing} />
-          {/* <Route path="/signup" component={Signup} />
+          <Route path="/signup" component={Signup} />
           <Route path="/login" component={Login} /> 
-          <Route path="/logout" component={Logout} /> */}
+          <Route path="/logout" component={Logout} />
 
         </Switch>
         </div>
