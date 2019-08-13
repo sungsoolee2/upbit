6bc4ff31eb50c60e1307d2603b0bb98f2a21c2f6
k-lia <k.liapoulos@gmail.com>
2019-08-12 22:14:52 -0500

    removed old routes

--- controllers/pricingController.js
+++ controllers/pricingController.js
@@ -38,64 +38,64 @@ module.exports = {
         })
     },
 
-    omenicsDataBTC: (req, res) => {
-        // console.log("KEY");
-        // res.send(authOMC);
-        // console.log("Requesting...", req);
-        // console.log("MOMENT JS " + moment().format("YYYY-MM-DD HH:mm"));
-        const currentTime = moment().format("YYYY-MM-DD");
-        console.log("MONTHS "+ moment().subtract(6, 'months').format("YYYY-MM-DD HH:mm:ss"))
-        const dayBefore = moment().subtract(1, 'days').format("YYYY-MM-DD")
-        const sixMonthsBefore = moment().subtract(6, 'months').format("YYYY-MM-DD");
-        axios.get("https://omenics.com/api/v1/BTC/"+dayBefore, { headers: { Authorization: 'Bearer '.concat(authOMC)}}).then(btc => {
-            //console.log(coinData.data);
-            // console.log(btc.data);
-            let data = Object.entries(btc.data);
-            // console.log(btc);
-            // console.log(Object.entries(btc.data));
-            let testData = [];
-            let parsedData = [];
-            let prices = [];
-            let volume = [];
-            let time = [];
+    // omenicsDataBTC: (req, res) => {
+    //     // console.log("KEY");
+    //     // res.send(authOMC);
+    //     // console.log("Requesting...", req);
+    //     // console.log("MOMENT JS " + moment().format("YYYY-MM-DD HH:mm"));
+    //     const currentTime = moment().format("YYYY-MM-DD");
+    //     console.log("MONTHS "+ moment().subtract(6, 'months').format("YYYY-MM-DD HH:mm:ss"))
+    //     const dayBefore = moment().subtract(1, 'days').format("YYYY-MM-DD")
+    //     const sixMonthsBefore = moment().subtract(6, 'months').format("YYYY-MM-DD");
+    //     axios.get("https://omenics.com/api/v1/BTC/"+dayBefore, { headers: { Authorization: 'Bearer '.concat(authOMC)}}).then(btc => {
+    //         //console.log(coinData.data);
+    //         // console.log(btc.data);
+    //         let data = Object.entries(btc.data);
+    //         // console.log(btc);
+    //         // console.log(Object.entries(btc.data));
+    //         let testData = [];
+    //         let parsedData = [];
+    //         let prices = [];
+    //         let volume = [];
+    //         let time = [];
 
-            for(let i = 0; i < data.length; i++){
-                // console.log(data[i]);
-                const test = Object.entries(data[i]);
-                let t = test[1][1].timestamp;
-                t = moment(t).format("YYYY-MM-DD HH:mm");
-                // console.log(test[1][1].timestamp);
-                parsedData.push({
-                    price: test[1][1].price_usd,
-                    volume_usd: test[1][1].volume_usd
-                    });
+    //         for(let i = 0; i < data.length; i++){
+    //             // console.log(data[i]);
+    //             const test = Object.entries(data[i]);
+    //             let t = test[1][1].timestamp;
+    //             t = moment(t).format("YYYY-MM-DD HH:mm");
+    //             // console.log(test[1][1].timestamp);
+    //             parsedData.push({
+    //                 price: test[1][1].price_usd,
+    //                 volume_usd: test[1][1].volume_usd
+    //                 });
                 
-                prices.push(test[1][1].price_usd);
-                volume.push(test[1][1].volume_usd);
-                time.push(t);
-                testData.push(test[1][1]);
-            }
-            // console.log(parsedData);
-            return res.json({prices: prices, volume: volume, time: time});
-        }).catch(err =>
-        {
-            console.log(err);
-            console.log("error :(");
-        })
-    },
+    //             prices.push(test[1][1].price_usd);
+    //             volume.push(test[1][1].volume_usd);
+    //             time.push(t);
+    //             testData.push(test[1][1]);
+    //         }
+    //         // console.log(parsedData);
+    //         return res.json({prices: prices, volume: volume, time: time});
+    //     }).catch(err =>
+    //     {
+    //         console.log(err);
+    //         console.log("error :(");
+    //     })
+    // },
 
-    omenicsDataBTCHistorical: (req, res) => {
-        //do calculation to access information within a 6 month period and store
-        //it into the db
+    // omenicsDataBTCHistorical: (req, res) => {
+    //     //do calculation to access information within a 6 month period and store
+    //     //it into the db
 
-        axios.get("https://omenics.com/api/v1/BTC/2019-03-01/2019-03-01", { headers: { Authorization: 'Bearer '.concat(authOMC)}}).then(btc => {
-            //console.log(coinData.data);
-            // console.log(btc);
-            return btc;
-        }).catch(err =>
-        {
-            console.log(err);
-            console.log("error :(");
-        })
-    }
+    //     axios.get("https://omenics.com/api/v1/BTC/2019-03-01/2019-03-01", { headers: { Authorization: 'Bearer '.concat(authOMC)}}).then(btc => {
+    //         //console.log(coinData.data);
+    //         // console.log(btc);
+    //         return btc;
+    //     }).catch(err =>
+    //     {
+    //         console.log(err);
+    //         console.log("error :(");
+    //     })
+    // }
 }
